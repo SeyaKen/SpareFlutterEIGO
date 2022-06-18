92cdae7bc24cf882a209a124c63aada617f37ff5
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-06-18 14:03:14 +0900

    Oh I really want to improve my Korean skils

--- lib/ScoreReport/Toeic_Report.dart
+++ lib/ScoreReport/Toeic_Report.dart
@@ -1,46 +1,16 @@
+
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
-
-class LineGraph extends StatefulWidget {
-  const LineGraph({ Key? key }) : super(key: key);
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
 
   @override
-  State<LineGraph> createState() => _LineGraphState();
+  State<MyWidget> createState() => _MyWidgetState();
 }
 
-class _LineGraphState extends State<LineGraph> {
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    body: Center(
-        child: Container(
-          child: SfCartesianChart(
-            // Initialize category axis
-            primaryXAxis: CategoryAxis(),
-
-            series: <LineSeries<SalesData, String>>[
-              LineSeries<SalesData, String>(
-                // Bind data source
-                dataSource:  <SalesData>[
-                  SalesData('Jan', 35),
-                  SalesData('Feb', 28),
-                  SalesData('Mar', 34),
-                  SalesData('Apr', 32),
-                  SalesData('May', 40)
-                ],
-                xValueMapper: (SalesData sales, _) => sales.year,
-                yValueMapper: (SalesData sales, _) => sales.sales
-              )
-            ]
-          )
-        )
-      )
-  );
+    
   }
-}
-class SalesData {
-  SalesData(this.year, this.sales);
-  final String year;
-  final double sales;
 }
