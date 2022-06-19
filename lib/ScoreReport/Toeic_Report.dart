f39481c1ab39de52eaea6c7f099c24fcd373c31d
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-06-19 13:31:19 +0900

    I cant wait!!

--- lib/ScoreReport/Toeic_Report.dart
+++ lib/ScoreReport/Toeic_Report.dart
@@ -9,19 +9,46 @@ class ToeicScreen extends StatefulWidget {
 }
 
 class _ToeicScreenState extends State<ToeicScreen> {
-  final List<Color> gradientsColors = [
-    const Color(0xff23b6e6),
-    const Color(0xff02d39a),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: LineChart(
         LineChartData(
-          
-        )
-      ),
+          minX: 0,
+          maxX: 11,
+          backgroundColor: const Color(0xff3800ff),
+          titlesData: LineTitles.getTitleData(),
+          gridData: FlGridData(
+              show: false,
+              ),
+          lineBarsData: [
+            LineChartBarData(spots: [
+              const FlSpot(0, 700),
+              const FlSpot(2, 710),
+              const FlSpot(4, 720),
+              const FlSpot(6, 730),
+              const FlSpot(8, 790),
+              const FlSpot(9, 900),
+              const FlSpot(11, 990),
+            ],
+            color: Colors.white,
+            barWidth: 5,
+            )
+          ])),
     );
   }
+}
+
+class LineTitles {
+  static getTittleData() => FlTitlesData(
+    show: true,
+    bottomTitles: SideTitles(
+      showTitles: true,
+      getTitlesWidget: (value) {
+        
+      },
+      margin: 8,
+    )
+  );
 }
